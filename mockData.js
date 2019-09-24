fafb3cead86b89611f427f66bd3880ed7ef3520e
Kozyrov <sfrankie11@gmail.com>
2019-09-23 22:30:49 -0500

    first commit - established port, copied mock data

---
+++ mockData.js
@@ -0,0 +1,82 @@
+const timesheets = {
+    '90034': {
+        workdays: [
+            {
+                name: 'Monday',
+                date: '9/9/2019',
+                value: 0
+            },
+            {
+                name: 'Tuesday',
+                date: '9/10/2019',
+                value: 0
+            },
+            {
+                name: 'Wednesday',
+                date: '9/11/2019',
+                value: 0
+            },
+            {
+                name: 'Thursday',
+                date: '9/12/2019',
+                value: 0
+            },
+            {
+                name: 'Friday',
+                date: '9/13/2019',
+                value: 0
+            },
+            {
+                name: 'Saturday',
+                date: '9/14/2019',
+                value: 0
+            },
+            {
+                name: 'Sunday',
+                date: '9/15/2019',
+                value: 0
+            }
+        ]
+    },
+    '55331': {
+        workdays: [
+            {
+                name: 'Monday',
+                date: '9/16/2019',
+                value: 0
+            },
+            {
+                name: 'Tuesday',
+                date: '9/17/2019',
+                value: 0
+            },
+            {
+                name: 'Wednesday',
+                date: '9/18/2019',
+                value: 0
+            },
+            {
+                name: 'Thursday',
+                date: '9/19/2019',
+                value: 0
+            },
+            {
+                name: 'Friday',
+                date: '9/20/2019',
+                value: 0
+            },
+            {
+                name: 'Saturday',
+                date: '9/21/2019',
+                value: 0
+            },
+            {
+                name: 'Sunday',
+                date: '9/22/2019',
+                value: 0
+            }
+        ]
+    }
+}
+
+module.exports = timesheets;
